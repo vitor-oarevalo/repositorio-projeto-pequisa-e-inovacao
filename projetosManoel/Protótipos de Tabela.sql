53d0e9d0b9196660cbdf7181153b56b363eff0b4
Manoel Alves <manoel.silvano@sptech.school>
2026-08-29 23:47:14 -0300

    Add files via upload

---
+++ projetosManoel/Protótipos de Tabela.sql
@@ -0,0 +1,46 @@
+use sprint1;
+
+create table cadastroProjeto(
+	idEmpresa int primary key auto_increment,
+    nome varchar(50),
+    cpnj char(18) unique
+);
+
+insert into cadastroProjeto values
+	(default, 'AkzoNobel', '52.872.147/0001-98');
+    
+select * from cadastroProjeto;
+insert into cadastroProjeto (nome, cpnj)values
+	('Lukscolor', '46.928.552/0001-65'),
+    ('Suvinil' ,'55.132.202/0001-89');
+
+use sprint1;
+
+create table registroSensor(
+	idRegistro int,
+    valorRegistro tinyint,
+    hrRegistro datetime
+);
+
+insert into registroSensor values
+	(1, 123, '2026-08-28 22:47:00');
+    
+select * from registroSensor;
+insert into registroSensor values 
+	(2, 89, '2025-09-08 12:09:47'),
+	(5, 95, '2025-01-12 09:45:28');
+
+use sprint1;
+
+create table SensorProjeto(
+	idSensor int primary key,
+    nome varchar(20),
+    dtInstalacao date
+);
+
+insert into SensorProjeto values 
+	(1, 'LM35', '2026-08-28');
+select * from SensorProjeto;
+insert into SensorProjeto values 
+	(2, 'LM35', '2025-09-08'),
+    (5, 'LM35' , '2025-01-12');
